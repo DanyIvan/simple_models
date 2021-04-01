694c587f54266f713c263dc4b99f8a5243ee1fda
Daniel <dany_ivan_gr@hotmail.com>
2021-04-01 10:05:46 -0600

    fix: increase time step, reverse order of execution and get name before saving

--- experiments_scripts/dynamic_stiff_solver.py
+++ experiments_scripts/dynamic_stiff_solver.py
@@ -4,22 +4,25 @@ from goldblatt import DynamicSteadyStates
 from utils.objects import save_object
 from datetime import datetime
 
-date = datetime.today().strftime('%Y%m%d_%H%M%S')
-NAME = 'dynamic_stiff_solver_' + date
+
 folder = '../experiments_data/'
 
-save_everys = [100, 100, 1000]
-times = [1e4, 1e5, 1e6]
+save_everys = [1000, 10000]
+dts = [10, 100]
+times = [1e6, 1e7]
 betas = [1e-1, 1e-2, 1e-3, 1e-4]
 rs = [1e10, 1e11, 1e12]
-for r in rs:
-    for beta in betas:
-        for idx, time in enumerate(times):
+
+for idx, time in enumerate(times):
+    for r in rs:
+        for beta in betas:
             print('r='+ format(r, 'E'))
             print('beta=' + format(beta, 'E'))
             print('time=' + format(time, 'E'))
-            states = DynamicSteadyStates(r=r, beta=beta, dt=0.1, total_time=time,
+            states = DynamicSteadyStates(r=r, beta=beta, dt=dts[idx], total_time=time,
                 save_every=save_everys[idx])
             states.find_steady_states()
+            date = datetime.today().strftime('%Y%m%d_%H%M%S')
+            NAME = 'dynamic_stiff_solver_' + date + '.obj'
             save_object(states, folder + NAME + '.obj')
 
