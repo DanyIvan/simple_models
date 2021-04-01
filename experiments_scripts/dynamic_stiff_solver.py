41648f31ec7e8c7afed94d9a08e41f45cec57eca
Daniel <dany_ivan_gr@hotmail.com>
2021-04-01 11:02:55 -0600

    fix: changes to accelerate process

--- experiments_scripts/dynamic_stiff_solver.py
+++ experiments_scripts/dynamic_stiff_solver.py
@@ -3,26 +3,24 @@ sys.path.append('..')
 from goldblatt import DynamicSteadyStates
 from utils.objects import save_object
 from datetime import datetime
+import numpy as np
 
 
 folder = '../experiments_data/'
 
-save_everys = [1000, 10000]
-dts = [10, 100]
-times = [1e6, 1e7]
-betas = [1e-1, 1e-2, 1e-3, 1e-4]
-rs = [1e10, 1e11, 1e12]
 
-for idx, time in enumerate(times):
-    for r in rs:
-        for beta in betas:
-            print('r='+ format(r, 'E'))
-            print('beta=' + format(beta, 'E'))
-            print('time=' + format(time, 'E'))
-            states = DynamicSteadyStates(r=r, beta=beta, dt=dts[idx], total_time=time,
-                save_every=save_everys[idx])
-            states.find_steady_states()
-            date = datetime.today().strftime('%Y%m%d_%H%M%S')
-            NAME = 'dynamic_stiff_solver_' + date + '.obj'
-            save_object(states, folder + NAME + '.obj')
+
+betas = [1e-2, 1e-3, 1e-4]
+rs = [3e10, 1e11, 1e12]
+
+for r in rs:
+    for beta in betas:
+        print('r='+ format(r, 'E'))
+        print('beta=' + format(beta, 'E'))
+        states = DynamicSteadyStates(r=r, beta=beta, dt=1, total_time=1e6,
+            save_every=1000)
+        states.find_steady_states(pp_list=np.logspace(13, 16, 50))
+        date = datetime.today().strftime('%Y%m%d_%H%M%S')
+        NAME = 'dynamic_stiff_solver_' + date + '.obj'
+        save_object(states, folder + NAME + '.obj')
 
