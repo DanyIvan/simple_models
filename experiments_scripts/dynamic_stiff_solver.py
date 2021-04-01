281f3649461aa107236a52d119fe9fa1f5fca91e
Daniel <dany_ivan_gr@hotmail.com>
2021-03-31 19:17:02 -0600

    feat: new experiment

---
+++ experiments_scripts/dynamic_stiff_solver.py
@@ -0,0 +1,15 @@
+import sys
+sys.path.append('..')
+from goldblatt import DynamicSteadyStates
+from utils.objects import save_object
+from datetime import datetime
+
+date = datetime.today().strftime('%Y%m%d_%H%M%S')
+NAME = 'dynamic_stiff_solver_' + date
+folder = '../experiments_data/'
+
+
+states = DynamicSteadyStates(r=3e10, beta=0.01, dt=0.1, total_time=1e4)
+states.find_steady_states()
+save_object(states, folder + NAME)
+
