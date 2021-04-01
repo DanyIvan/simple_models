e9109ac08d1898900188cb06ee97fb23f793e533
Daniel <dany_ivan_gr@hotmail.com>
2021-03-31 23:20:10 -0600

    fix: vary r, beta and time

--- experiments_scripts/dynamic_stiff_solver.py
+++ experiments_scripts/dynamic_stiff_solver.py
@@ -8,9 +8,18 @@ date = datetime.today().strftime('%Y%m%d_%H%M%S')
 NAME = 'dynamic_stiff_solver_' + date
 folder = '../experiments_data/'
 
-
-states = DynamicSteadyStates(r=3e10, beta=0.01, dt=0.1, total_time=1e5,
-    save_every=100)
-states.find_steady_states()
-save_object(states, folder + NAME + '.obj')
+save_everys = [100, 100, 1000]
+times = [1e4, 1e5, 1e6]
+betas = [1e-1, 1e-2, 1e-3, 1e-4]
+rs = [1e10, 1e11, 1e12]
+for r in rs:
+    for beta in betas:
+        for idx, time in enumerate(times):
+            print('r='+ format(r, 'E'))
+            print('beta=' + format(beta, 'E'))
+            print('time=' + format(time, 'E'))
+            states = DynamicSteadyStates(r=r, beta=beta, dt=0.1, total_time=time,
+                save_every=save_everys[idx])
+            states.find_steady_states()
+            save_object(states, folder + NAME + '.obj')
 
